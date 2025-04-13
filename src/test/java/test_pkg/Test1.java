7ac3c8c583a15a72eb4ad0bdb64e29f771a48a96
Shivam Hajare <shivamhajare96@gmail.com>
2025-04-13 18:24:15 +0530

    testng xml file added

---
+++ src/test/java/test_pkg/Test1.java
@@ -0,0 +1,12 @@
+package test_pkg;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+	
+	@Test
+	public void Test1() {
+		System.out.println("#############  This is test 1 running!!!!!!!!!!!!!!");
+	}
+
+}
---
+++ src/test/java/test_pkg/Test2.java
@@ -0,0 +1,12 @@
+package test_pkg;
+
+import org.testng.annotations.Test;
+
+public class Test2 {
+	
+	@Test
+	public void Test2() {
+		System.out.println("@@@@@@@@@@@@@@@@@  This is test 2 running!!!!!!!!!!!!!!");
+	}
+
+}
